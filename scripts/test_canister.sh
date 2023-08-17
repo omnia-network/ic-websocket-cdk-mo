fc3b17bab3de0a7bc0ef80bb51cf27875d49fbec
Luca8991 <luca.berte98@hotmail.it>
2023-08-17 20:05:17 +0200

    fix: integration tests environment

--- scripts/test_canister.sh
+++ scripts/test_canister.sh
@@ -10,13 +10,17 @@ mops install
 # install Node.js dependencies
 npm install
 
-# integration tests
+# prepare environment for integration tests
 dfx start --clean --background
 
 npm run deploy:tests
 
 npm run generate
 
+# get the canister id of the test canister and save it to .env
+echo "CANISTER_ID_TEST_CANISTER='$(npx json -f .dfx/local/canister_ids.json test_canister.local)'" > .env
+
+# run integration tests
 npm run test:integration
 
 dfx stop
